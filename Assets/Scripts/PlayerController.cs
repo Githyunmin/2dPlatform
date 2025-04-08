e4fe3b140354894eea376ffe857aa2a00b4b6f1b
Githyunmin <hmkoh25@g.shingu.ac.kr>
2025-04-08 16:14:04 +0900

    [Feat] midle

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -13,11 +13,13 @@ public class PlayerController : MonoBehaviour
     public LayerMask groundLayer;
 
     private Rigidbody2D rb;
+    private Animator pAni;
     private bool isGrounded;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        pAni = GetComponent<Animator>();
     }
 
 
@@ -26,11 +28,22 @@ public class PlayerController : MonoBehaviour
         float moveInput = Input.GetAxisRaw("Horizontal");
         rb.velocity = new Vector2(moveInput * moveSpeed, rb.velocity.y);
 
+        if (moveInput < 0)
+        {
+            transform.localScale = new Vector3(1f, 1f, 1f);
+        }
+
+        if (moveInput > 0)
+        {
+            transform.localScale = new Vector3(-1f, 1f, 1f);
+        }
+
         isGrounded = Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
 
         if (isGrounded && Input.GetKeyDown(KeyCode.Space))
         {
             rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            pAni.SetTrigger("JumpAction");
         }
     }
 
@@ -46,6 +59,11 @@ public class PlayerController : MonoBehaviour
         if (collision.CompareTag("Finish"))
         {
             collision.GetComponent<LevelObject>().MoveToNextLevel();
+        }
+
+        if (collision.CompareTag("Enemy"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         }
 
     }
